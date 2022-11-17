be16bbc5f4490f5dbf91572292abfc58aed6a6e8
Jessie <jessieguo.05@gmail.com>
2022-11-17 20:41:16 +0000

    Kivy practice - mouse inputs

--- kivy-prototype/kivy-tutorials/mouse-touch-input/mouse_touch.py
+++ kivy-prototype/kivy-tutorials/mouse-touch-input/mouse_touch.py
@@ -1,19 +1,27 @@
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
 
 class Touch(Widget):
-    def on_touch_down(self, touch):
-        pass
+    btn = ObjectProperty(None)
+    def on_touch_down(self, touch): #these methods inherit from widget, so we override
+        print("Mouse down", touch)
+        self.btn.opacity = 0.5
 
     def on_touch_move(self, touch):
-        pass
+        print("Mouse move", touch)
 
     def on_touch_up(self, touch):
-        pass
+        print("Mouse up", touch)
+        self.btn.opacity = 1
 
-class UserInputs(App):
+class AppInputs(App):
     def build(self):
-        return 
+        return Touch()
 
 if __name__ == "__main__":
-    UserInputs().run()
+    AppInputs().run()
